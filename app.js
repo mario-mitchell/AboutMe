1c6eea179f18ce0bbb2317bda10e3748063343a7
Mario Mitchell <35703472+mario-mitchell@users.noreply.github.com>
2023-08-28 16:39:18 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const moreFactsButton = document.getElementById("moreFactsButton");
+  
+    moreFactsButton.addEventListener("click", function () {
+      window.location.href = "index2.html";
+    });
+  });
+  
